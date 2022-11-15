8ad7f968346cf0e4e91a7df1f71de8d6a2765cc6
Elchanan <elchanan4567@gmail.com>
2022-11-15 11:54:22 +0200

    explore real data

--- data/examples.py
+++ data/examples.py
@@ -1,8 +1,12 @@
 import logging
+from typing import Set, Tuple
+import itertools
 
 import numpy as np
-import pandas as pd
+import torch
 import matplotlib.pyplot as plt
+
+#from nets import co
 
 
 def create_block_matrix(batch_size: int, blocks_num: int, block_size: int, epsilon: float,
@@ -10,7 +14,7 @@ def create_block_matrix(batch_size: int, blocks_num: int, block_size: int, epsil
     l = blocks_num * block_size
     if seed >= 0:
         np.random.seed(seed)
-    m = (np.random.rand(batch_size, 1, l, l) < epsilon/2.0)
+    m = (np.random.rand(batch_size, 1, l, l) < epsilon / 2.0)
     y, x = np.meshgrid(range(l), range(l))
     mask = y // block_size == x // block_size
     mask = np.expand_dims(mask, [0, 1])
@@ -19,5 +23,33 @@ def create_block_matrix(batch_size: int, blocks_num: int, block_size: int, epsil
     m[mask] = True
     m = (m | np.transpose(m, [0, 1, 3, 2])).astype(int)
     m = m - np.eye(l)
+
     # logging.info(f"shape: {m.shape}")
     return m
+
+
+def find_colissions(collision_matrix: np.ndarray, paths_dist: np.ndarray) -> Set[Tuple[int, int]]:
+    rs, cs = np.where(collision_matrix > 0)
+    coordinates = list(zip(rs, cs))
+    ps = np.where(paths_dist.flatten() > 0.5)[0]
+    pairs = list(itertools.combinations(ps, 2))
+    return set(pairs) & set(coordinates)
+
+
+def count_collisions(collision_matrix: np.ndarray, paths_dist: np.ndarray) -> int:
+    s = (paths_dist.flatten() > 0.5).astype(float)
+    return int(s @ collision_matrix @ s)
+
+
+def greedy_repair(collision_matrix: np.ndarray, paths_dist: np.ndarray) -> np.ndarray:
+    collisions = find_colissions(collision_matrix, paths_dist)
+    blocks_num, block_size = collision_matrix.shape
+    rs = []
+    cs = []
+    for c in collisions:
+        rs.append(c // block_size)
+        cs.append(c % block_size)
+    paths_dist[list(rs), list(cs)] = 0
+    for r in rs:
+        collision_row = collision_matrix[r]
+
--- nets/co.py
+++ nets/co.py
@@ -1,45 +1,66 @@
 import logging
-
+from typing import Dict
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-from data.examples import create_block_matrix
+from data import examples
 from utils import set_log
 
 
 def get_real_val(r_weights: torch.Tensor, block: torch.Tensor) -> float:
     r_weights = r_weights.detach().numpy()
     block = block.numpy().squeeze()
-    inds = np.argmax(r_weights, axis=1).flatten()
+    maxes = np.argmax(r_weights, axis=1).flatten()
     selects = np.zeros_like(r_weights)
-    selects[:,inds]=1
+    block_inds = range(r_weights.shape[0])
+    selects[block_inds, maxes] = 1
     selects = selects.flatten()
     return np.dot(selects, np.dot(block, selects))
 
 
-def optimize(weights: torch.Tensor, block: torch.Tensor):
+def optimize(block: torch.Tensor, blocks_num: int, block_size: int) -> Dict[float, np.ndarray]:
+    # weights_num = block.shape[-1]
+    weights = torch.zeros(blocks_num * block_size, requires_grad=True)
     optimizer = torch.optim.Adam([weights], lr=1e-2)
-    ones = torch.ones(300).float()
+    one_weights = torch.ones(blocks_num * block_size) / block_size
+    res_dict = {}
     for i in range(10000):
         optimizer.zero_grad()
-        r_weights = weights.view(30, 10)
-        negative_loss = torch.nn.ReLU()(-r_weights).sum()
-        distribution_loss = torch.pow(r_weights.sum(axis=1) - 1, 2).sum()
-        collision_loss = torch.mean(torch.mul(torch.matmul(weights, block.squeeze()), ones))
-        bl = torch.mean(torch.mul(torch.matmul(ones, block.squeeze()), ones))
-        loss = negative_loss + distribution_loss + collision_loss
+        # r_weights = weights.view(block.shape[-2], block.shape[-1])
+        negative_loss = torch.nn.ReLU()(-weights).sum()
+        dist_weights = torch.reshape(weights, (blocks_num, block_size))
+        distribution_loss = torch.pow(dist_weights.sum(axis=1) - 1, 2).sum()
+        collision_loss = torch.matmul(torch.matmul(weights, block.squeeze()), weights)**2
+        bl = torch.mul(torch.matmul(one_weights, block.squeeze()), one_weights)
+        loss = negative_loss + distribution_loss + 0.1 * collision_loss
         loss.backward()
         optimizer.step()
-        if i % 1000 == 0:
-            logging.info(f"real_val: {get_real_val(r_weights, block)}")
-            logging.info(
-                f"bl: {bl}, negative_loss: {negative_loss.item()}, distribution loss: {distribution_loss.item()}, collision loss: {collision_loss.item()}")
+        if i % 1 == 0:
+            # logging.info()
+            real_val = get_real_val(dist_weights, block)
+            # logging.info(f"real_val: {get_real_val(dist_weights, block)}")
+            if real_val not in res_dict:
+                res_dict[real_val] = dist_weights.detach().numpy()
+                logging.info(
+                    f"ind:  {i}, real_val: {real_val}, loss: {loss}, negative_loss: {negative_loss}, dist_loss: {distribution_loss}, colission loss: {0.1 * collision_loss}")
+    return res_dict
+    # logging.info(
+    #     f"bl: {bl}, negative_loss: {negative_loss.item()}, distribution loss: {distribution_loss.item()}, collision loss: {collision_loss.item()}")
 
 
 if __name__ == "__main__":
     set_log()
-    weights = torch.zeros(300, requires_grad=True)
-    #ones = torch.ones(300).float() / 10
-    block = create_block_matrix(batch_size=1, blocks_num=30, block_size=10, epsilon=0.1, seed=0)
+    blocks_num = 30
+    block_size = 10
+    batch_size = 16
+    epsilon = 0.1
+    seed = -1
+
+    weights = torch.zeros(blocks_num * block_size, requires_grad=True)
+
+    # ones = torch.ones(300).float() / 10
+    block = examples.create_block_matrix(batch_size=1, blocks_num=blocks_num, block_size=block_size, epsilon=0.1,
+                                seed=-1).squeeze()
     block = torch.tensor(block).float()
-    optimize(weights, block)
+    res_dict = optimize(block, blocks_num=blocks_num, block_size=block_size)
+    examples.find_colissions(block.numpy(), res_dict[4])
